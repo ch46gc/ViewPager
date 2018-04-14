a8154cf841916055e79734aa03d5d266fbab1059
ch46gc <chriseyhillgen@gmail.com>
2018-04-14 10:42:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.viewpager;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        getSupportActionBar().setElevation(0);
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+    }
+
+}
